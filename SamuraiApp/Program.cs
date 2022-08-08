f8efd81d41dbc19f0ca2a96e98c56650251c52d5
ayoubzann <ayoubzann@gmail.com>
2022-08-08 14:17:59 +0200

    Added Console App

---
+++ SamuraiApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SamuraiApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
